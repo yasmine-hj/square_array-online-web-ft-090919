4a4768f1d60eedf4d27c134e245b07fc6a65616a
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-11 11:22:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
   array.each do |number|
-    new_array<< k#{number}**2
+    new_array<<#{number}**2
     counter +=1
     return new_array
   end
